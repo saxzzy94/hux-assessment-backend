2bc0a18635a8edba6441c1e9bf6cc62d95cd45b1
Osazee Agbonze <osazee@altum.ai>
2024-07-22 14:58:42 +0100

    fix cors issues

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { config } from "dotenv";
 import userRoute from "./interfaces/http/routes/user";
 import contactRoute from "./interfaces/http/routes/contact";
 import { setupSwagger } from "./swagger";
+import cors from 'cors'
 
 config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use('/api', userRoute);
 app.use('/api', contactRoute);
